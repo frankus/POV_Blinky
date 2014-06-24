eb862d2a36b42168c98d5cd708fda5fa703dba44
Frank Schmitt <frank@frankschmitt.org>
2014-06-23 20:36:14 -0700

    initial commit

---
+++ POVBlinky.ino
@@ -0,0 +1,564 @@
+unsigned char blank = B00000;
+
+unsigned char unprintable[5] = {
+  B11111,
+  B10001,
+  B10001,
+  B10001,
+  B11111
+};
+
+unsigned char space[5] = {
+  blank,
+  blank,
+  blank,
+  blank,
+  blank
+};
+
+unsigned char exclamation_mark[5] = {
+  blank,
+  blank,
+  B11101,
+  blank,
+  blank
+};
+
+unsigned char double_quote[5] = {
+  blank,
+  B11000,
+  blank,
+  B11000,
+  blank
+};
+
+unsigned char octothorpe[5] = {
+  B01010,
+  B11111,
+  B01010,
+  B11111,
+  B01010
+};
+
+unsigned char dollar_sign[5] = {
+  B01001,
+  B10101,
+  B11111,
+  B10101,
+  B10010
+};
+
+unsigned char percent_sign[5] = {
+  B11001,
+  B11010,
+  B00100,
+  B01011,
+  B10011
+};
+
+unsigned char ampersand[5] = {
+  B00010,
+  B01101,
+  B10101,
+  B01010,
+  B00001
+};
+
+unsigned char single_quote[5] = {
+  blank,
+  blank,
+  B11000,
+  blank,
+  blank
+};
+
+unsigned char open_parenthesis[5] = {
+  blank,
+  blank,
+  B01110,
+  B10001,
+  blank
+};
+
+unsigned char close_parenthesis[5] = {
+  blank,
+  B10001,
+  B01110,
+  blank,
+  blank
+};
+
+unsigned char asterisk[5] = {
+  B00100,
+  B10101,
+  B01110,
+  B10101,
+  B00100
+};
+
+unsigned char plus_sign[5] = {
+  B00100,
+  B00100,
+  B11111,
+  B00100,
+  B00100
+};
+
+unsigned char comma[5] = {
+  blank,
+  B00001,
+  B00010,
+  blank,
+  blank
+};
+
+unsigned char minus_sign[5] = {
+  B00100,
+  B00100,
+  B00100,
+  B00100,
+  B00100
+};
+
+unsigned char period[5] = {
+  blank,
+  B00001,
+  blank,
+  blank,
+  blank
+};
+
+unsigned char forward_slash[5] = {
+  B00001,
+  B00010,
+  B00100,
+  B01000,
+  B10000
+};
+
+unsigned char zero[5] = {
+  B01110,
+  B10001,
+  B10101,
+  B10001,
+  B01110
+};
+
+unsigned char one[5] = {
+  blank,
+  B01001,
+  B11111,
+  B00001,
+  blank
+};
+
+unsigned char two[5] = {
+  B01001,
+  B10011,
+  B10101,
+  B10101,
+  B01001
+};
+
+unsigned char three[5] = {
+  B01010,
+  B10001,
+  B10101,
+  B10101,
+  B01010
+};
+
+unsigned char four[5] = {
+  B00010,
+  B00110,
+  B01010,
+  B11111,
+  B00010
+};
+
+unsigned char five[5] = {
+  B11101,
+  B10101,
+  B10101,
+  B10101,
+  B10010
+};
+
+unsigned char six[5] = {
+  B01110,
+  B10101,
+  B10101,
+  B10101,
+  B10010
+};
+
+unsigned char seven[5] = {
+  B10000,
+  B10000,
+  B10111,
+  B11000,
+  B00000
+};
+
+unsigned char eight[5] = {
+  B01010,
+  B10101,
+  B10101,
+  B10101,
+  B01010,
+};
+
+unsigned char nine[5] = {
+  B01000,
+  B10101,
+  B10101,
+  B10101,
+  B01110
+};
+
+unsigned char colon[5] = {
+  blank,
+  B01010,
+  blank,
+  blank,
+  blank
+};
+
+unsigned char semicolon[5] = {
+  B00001,
+  B01010,
+  blank,
+  blank,
+  blank,
+};
+
+unsigned char less_than_sign[5] = {
+  blank,
+  B00100,
+  B01010,
+  B10001,
+  blank
+};
+
+unsigned char equal_sign[5] = {
+  B01010,
+  B01010,
+  B01010,
+  B01010,
+  B01010
+};
+
+unsigned char greater_than_sign[5] = {
+  blank,
+  B10001,
+  B01010,
+  B00100,
+  blank,
+};
+
+unsigned char question_mark[5] = {
+  B01000,
+  B10000,
+  B10101,
+  B10100,
+  B01000
+};
+
+unsigned char at_sign[5] = {
+  B01110,
+	B10000,
+	B10111,
+	B10101,
+	B01111
+};
+
+unsigned char uppercase_a[5] = {
+	B01111,
+	B10100,
+	B10100,
+	B10100,
+	B01111
+};
+
+unsigned char uppercase_b[5] = {
+	B11111,
+	B10101,
+	B10101,
+	B10101,
+	B01010
+};
+
+unsigned char uppercase_c[5] = {
+	B01110,
+	B10001,
+	B10001,
+	B10001,
+	B10001
+};
+
+unsigned char uppercase_d[5] = {
+	B11111,
+	B10001,
+	B10001,
+	B10001,
+	B01110
+};
+
+unsigned char uppercase_e[5] = {
+	B11111,
+	B10101,
+	B10101,
+	B10101,
+	B10001
+};
+
+unsigned char uppercase_f[5] = {
+	B11111,
+	B10100,
+	B10100,
+	B10100,
+	B10000
+};
+
+unsigned char uppercase_g[5] = {
+	B01110,
+	B10001,
+	B10001,
+	B10101,
+	B10111
+};
+
+unsigned char uppercase_h[5] = {
+	B11111,
+	B00100,
+	B00100,
+	B00100,
+	B11111
+};
+
+unsigned char uppercase_i[5] = {
+	B00000,
+	B10001,
+	B11111,
+	B10001,
+	B00000
+};
+
+unsigned char uppercase_j[5] = {
+	B00010,
+	B00001,
+	B10001,
+	B11110,
+	B10000
+};
+
+unsigned char uppercase_k[5] = {
+	B11111,
+	B00100,
+	B00100,
+	B01010,
+	B10001
+};
+
+unsigned char uppercase_l[5] = {
+	B11111,
+	B00001,
+	B00001,
+	B00001,
+	B00001
+};
+
+unsigned char uppercase_m[5] = {
+	B11111,
+	B01000,
+	B00100,
+	B01000,
+	B11111
+};
+
+unsigned char uppercase_n[5] = {
+	B11111,
+	B01000,
+	B00100,
+	B00010,
+	B11111
+};
+
+unsigned char uppercase_o[5] = {
+	B01110,
+	B10001,
+	B10001,
+	B10001,
+	B01110
+};
+
+unsigned char uppercase_p[5] = {
+	B11111,
+	B10100,
+	B10100,
+	B10100,
+	B01000
+};
+
+unsigned char uppercase_q[5] = {
+	B01110,
+	B10001,
+	B10101,
+	B10011,
+	B01111
+};
+
+unsigned char uppercase_r[5] = {
+	B11111,
+	B10100,
+	B10100,
+	B10100,
+	B01011
+};
+
+unsigned char uppercase_s[5] = {
+	B01001,
+	B10101,
+	B10101,
+	B10101,
+	B10010
+};
+
+unsigned char uppercase_t[5] = {
+	B10000,
+	B10000,
+	B11111,
+	B10000,
+	B10000
+};
+
+unsigned char uppercase_u[5] = {
+	B11110,
+	B00001,
+	B00001,
+	B00001,
+	B11110
+};
+
+unsigned char uppercase_v[5] = {
+	B11000,
+	B00110,
+	B00001,
+	B00110,
+	B11000
+};
+
+unsigned char uppercase_w[5] = {
+	B11100,
+	B00011,
+	B11100,
+	B00011,
+	B11100
+};
+
+unsigned char uppercase_x[5] = {
+	B10001,
+	B01010,
+	B00100,
+	B01010,
+	B10001
+};
+
+unsigned char uppercase_y[5] = {
+	B10000,
+	B01000,
+	B00111,
+	B01000,
+	B10000
+};
+
+unsigned char uppercase_z[5] = {
+	B10001,
+	B10011,
+	B10101,
+	B11001,
+	B10001
+};
+
+unsigned char open_bracket[5] = {
+	blank,
+	blank,
+	B11111,
+	B10001,
+	blank
+};
+
+unsigned char back_slash[5] = {
+	B10000,
+	B01000,
+	B00100,
+	B00010,
+	B00001
+};
+
+unsigned char close_bracket[5] = {
+  blank,
+  B10001,
+	B11111,
+	blank,
+	blank
+};
+
+unsigned char caret[5] = {
+	blank,
+	B01000,
+	B10000,
+	B01000,
+	blank
+};
+
+unsigned char underscore[5] = {
+	B00001,
+	B00001,
+	B00001,
+	B00001,
+	B00001
+};
+
+unsigned char* ASCII[] = {
+  space, exclamation_mark, double_quote, octothorpe, dollar_sign, percent_sign, ampersand, single_quote,
+  open_parenthesis, close_parenthesis, asterisk, plus_sign, comma, minus_sign, period, forward_slash,
+  zero, one, two, three, four, five, six, seven, 
+  eight, nine, colon, semicolon, less_than_sign, equal_sign, greater_than_sign, question_mark,
+
+  at_sign, uppercase_a, uppercase_b, uppercase_c, uppercase_d, uppercase_e, uppercase_f, uppercase_g,
+  uppercase_h, uppercase_i, uppercase_j, uppercase_k, uppercase_l, uppercase_m, uppercase_n, uppercase_o,
+  uppercase_p, uppercase_q, uppercase_r, uppercase_s, uppercase_t, uppercase_u, uppercase_v, uppercase_w,
+  uppercase_x, uppercase_y, uppercase_z, open_bracket, back_slash, close_bracket, caret, underscore,
+};
+
+unsigned char character_width = 5;
+int pixel_delay = 3;
+int letter_spacing = 1;
+
+void setup() {                
+  // initialize the digital pin as an output.
+  DDRB = DDRB | B00011111;
+}
+
+void loop() {
+	char *string = "HELLO WORLD!";
+
+	for (int i = 0; i < strlen(string); i ++) {
+		unsigned char *letter;
+		if (string[i] < 32 || string[i] > 126)
+			letter = unprintable;
+		else
+			letter = ASCII[string[i] - 32];
+				
+		for (int j = 0; j < character_width; j ++) {
+			PORTB = letter[j] & B00011111;
+			delay(pixel_delay);
+		}
+		
+		PORTB = blank & B00011111;
+		delay(pixel_delay * letter_spacing);
+	}
+	
+	delay(pixel_delay * (character_width + letter_spacing) * 3);
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+#POV Blinky
+
+This is an arduino sketch that will let you output a series of 5x5 characters to a 5-LED array attached to an ATTiny85 MCU, for example [this hardware design](http://upverter.com/frankus/01285dd0de2620e6/POV-BLINKY/).
+
